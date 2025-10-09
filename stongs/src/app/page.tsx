ee88f17d1871f4900985c0ce624408c750eb726e
sana-3737 <sana.fuwaru@gmail.com>
2025-10-09 13:56:47 +0900

    CSS

--- stongs/src/app/page.tsx
+++ stongs/src/app/page.tsx
@@ -5,7 +5,7 @@ import Linkspace from '@/components/linkspace';'./components/linkspace.tsx';
  
 export default function Home() {
   return (
-   <main className="min-h-screen max-w-[900px] max-h-[600px] mx-auto rounded-lg bg-[#e6e6fa] p-6 shadow-md">
+   <main className="min-h-screen max-w-[900px] mx-auto rounded-lg bg-[#e6e6fa] p-6 shadow-md pb-24">
   <h1 className="text-xl font-bold mb-6 text-center">わくわく シャッフルプレイリスト🎶</h1>
 
   <div className="flex flex-col md:flex-row gap-6 max-w-5xl mx-auto">
@@ -14,7 +14,7 @@ export default function Home() {
       <Playlist />
     </div>
     
-    <div className="md:w-1/3">
+    <div className="md:w-1/3 max-h-[calc(100vh-200px)]">
       <Linkspace />
     </div>
   </div>
